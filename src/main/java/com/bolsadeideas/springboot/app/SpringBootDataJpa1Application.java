cfcccf6b9208e7b70c7da5b67f2dd125cab898bf
Josue <jsuegr@gmail.com>
2018-03-11 14:30:29 -0600

    initial commit

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpa1Application.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpa1Application.class, args);
+	}
+}
