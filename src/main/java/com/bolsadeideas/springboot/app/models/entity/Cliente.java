9cbac6cd0e26073910a89b61bb56626e3640a0a3
Josue <jsuegr@gmail.com>
2018-03-12 17:18:55 -0600

    Clase 64: Agregando fechas en formulario

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -12,6 +12,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 @Table(name="clientes")
 public class Cliente implements Serializable{
@@ -26,9 +28,8 @@ public class Cliente implements Serializable{
 	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
-	
-	
 	
 	public Long getId() {
 		return id;
