f8af87f787b14c26e20de77900fb7f4056d04c1e
Josue <jsuegr@gmail.com>
2018-03-13 21:13:22 -0600

    Clase 83: AGREGANDO CAMPO FILE EN FORMULARIO Y PROCESAR MULTIPARTFILE EN EL CONTROLADOR

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,9 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
@@ -68,10 +73,29 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash,SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,@RequestParam("file") MultipartFile foto 
+			,RedirectAttributes flash,SessionStatus status) {
 		if(result.hasErrors()){
 			model.addAttribute("titulo","Formulario de cliente");
 			return "form";
+		}
+		
+		if(!foto.isEmpty())
+		{
+			Path directorioRecursos = Paths.get("src//main//resources/static/uploads");
+			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			try {
+				byte[] bytes = foto.getBytes();
+				Path rutaCompleta = Paths.get(rootPath+"//"+foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				
+				flash.addFlashAttribute("info","Se ha subido correctamente '"+ foto.getOriginalFilename() + "'");
+				
+				cliente.setFoto(foto.getOriginalFilename());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		String mensajeFlash = (cliente.getId() != null)? "El cliente ha sido editado con éxito": "El cliente ha sido creado con éxito."; 
