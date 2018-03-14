8952900109c91d04c2732518e4b1dcded365e6fd
Josue <jsuegr@gmail.com>
2018-03-14 10:06:36 -0600

    Clase 88: CARGANDO IMAGEN PROGRAMÁTICAMENTE EN LA RESPUESTA HTTP - CON RESPONSEENTITY

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 public class MvcConfig extends WebMvcConfigurerAdapter{
-
+/*
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
@@ -16,6 +16,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter{
 			.addResourceLocations(resourcePath);
 		
 	}
-
+*/
 	
 }
