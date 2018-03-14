5a545b02232c401dac7949938f90b67e492dc310
Josue <jsuegr@gmail.com>
2018-03-13 20:27:44 -0600

    Clase 82: ANTES DE EMPEZAR - DEMOSTRACIÓN Y PREPARANDO LA APLICACIÓN

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -39,6 +39,8 @@ public class Cliente implements Serializable{
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
+	private String foto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -70,5 +72,12 @@ public class Cliente implements Serializable{
 		this.createAt = createAt;
 	}
 	
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
 	private static final long serialVersionUID = 1L;
 }
